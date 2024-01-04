4891834934f159e62463152ba0fb8a453e85cbd6
Sayak-Bal <sayakbal005@gmail.com>
2024-01-04 21:55:35 +0530

    fist upload

---
+++ index.js
@@ -0,0 +1,9 @@
+
+function showsidebar(){
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = "flex";
+}
+function showhidebar(){
+    const hidebar =document.querySelector('.sidebar');
+    hidebar.style.display = "none";
+}
